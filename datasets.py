2a783bdfc01d83c4ceb3195ec19e54320a8c242e
newfyu <lhan12345@hotmail.com>
2021-05-29 10:53:31 +0800

    ready update to mlflow logger

--- datasets.py
+++ datasets.py
@@ -18,9 +18,9 @@ class ImageDataset(Dataset):
         item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
 
         if self.unaligned:
-            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]).convert('RGB'))
+            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]).convert('L'))
         else:
-            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]).convert('RGB'))
+            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]).convert('L'))
 
         return {'A': item_A, 'B': item_B}
 
--- train.py
+++ train.py
@@ -16,12 +16,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
+parser.add_argument('--dataroot', type=str, default='datasets/cycledsa_v1/', help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
 parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
-parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
+parser.add_argument('--input_nc', type=int, default=1, help='number of channels of input data')
+parser.add_argument('--output_nc', type=int, default=1, help='number of channels of output data')
 parser.add_argument('--cuda', action='store_true', help='use GPU computation')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--name', type=str, help='trial name', required=True)
@@ -96,11 +96,11 @@ fake_A_buffer = ReplayBuffer()
 fake_B_buffer = ReplayBuffer()
 
 # Dataset loader
-transforms_ = [transforms.Resize(int(opt.size * 1.12), Image.BICUBIC),
-               transforms.RandomCrop(opt.size),
-               transforms.RandomHorizontalFlip(),
+transforms_ = [transforms.RandomResizedCrop(256,scale=(0.6,1.4),interpolation=Image.BICUBIC),
+               transforms.ColorJitter(0.2,0.2),
                transforms.ToTensor(),
-               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+               transforms.Normalize((0.5,),(0.5,)),
+              ]
 dataloader = DataLoader(ImageDataset(opt.dataroot, transforms_=transforms_, unaligned=True),
                         batch_size=opt.batchSize, shuffle=True, num_workers=opt.n_cpu)
 
